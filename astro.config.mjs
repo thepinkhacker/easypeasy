a32bb9894452f1e6adf50395f5465adb29ba1e37
Kirstin Kleyn <kirstin.kleyn@fullstack.co.za>
2026-08-19 09:29:47 +0200

    Add blog functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,29 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import decapCmsOauth from 'astro-decap-cms-oauth';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aiwithease.net',
+
+  output: 'server',
+
   integrations: [
     sitemap({
-      filter: (page) => !page.includes('/questionnaire'),
+      filter: (page) =>
+        !page.includes('/questionnaire') &&
+        !page.includes('/admin') &&
+        !page.includes('/oauth'),
     }),
+    decapCmsOauth(),
   ],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
